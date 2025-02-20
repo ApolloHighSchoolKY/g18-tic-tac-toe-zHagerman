cacbfefcd22b1b5aa44fe3e9a298835bf38cd010
zHagerman <zander.hagerman@stu.daviess.kyschools.us>
2025-02-20 03:34:09 +0000

    mostly done

--- Lab24d.java
+++ Lab24d.java
@@ -10,16 +10,40 @@ import java.util.Scanner;
 
 public class Lab24d
 {
-	public static void main( String args[] ) throws IOException
+	public static void main(String[] args) throws IOException
 	{
 		Scanner file = new Scanner (new File("lab24d.dat"));
 
+		//Read how many times you should read
 
+			TicTacToe ttt = new TicTacToe("XXXOOXXOO");
+			System.out.println(ttt.getWinner());
+			System.out.println(ttt.toString());
+			
+			TicTacToe ttt2 = new TicTacToe("OXOOXOXOX");
+			System.out.println(ttt2.getWinner());
+			System.out.println(ttt2.toString());
 
+			TicTacToe ttt3 = new TicTacToe("OXOXXOXOO");
+			System.out.println(ttt3.getWinner());
+			System.out.println(ttt3.toString());
 
+			TicTacToe ttt4 = new TicTacToe("OXXOXOXOO");
+			System.out.println(ttt4.getWinner());
+			System.out.println(ttt4.toString());
 
+			TicTacToe ttt5 = new TicTacToe("XOXOOOXXO");
+			System.out.println(ttt5.getWinner());
+			System.out.println(ttt5.toString());
+			
+			//Tic tac toe only wants this file.nextline()	
+			
+			//Make tic tac toe object
 
+			//print the game
 
+			//Print the winner
+		
 
 	}
 }
--- TicTacToe.java
+++ TicTacToe.java
@@ -1,9 +1,10 @@
-//© A+ Computer Science  -  www.apluscompsci.com
+//ï¿½ A+ Computer Science  -  www.apluscompsci.com
 //Name -
 //Date -
 //Class - 
 //Lab  -
 
+import java.util.Arrays;
 import java.util.Scanner;
 import static java.lang.System.*;
 
@@ -13,42 +14,64 @@ public class TicTacToe
 
 	public TicTacToe()
 	{
-
+		mat = new char[3][3];
 
 	}
 
 	public TicTacToe(String game)
 	{
-
-
-
-
+		char[][] fat = new char[3][3];
+		int count = 0;
+		//Row major order will happen here
+		for(int row = 0; row <fat.length; row++)
+		{
+			
+			for(int col = 0; col < fat[row].length; col++)
+			{
+				fat[row][col] = (game.charAt(count));
+				count++;
+			}
+			
+		}
+		mat = fat;
 	}
 
 	public String getWinner()
 	{
+		char[][]mat1 = new char[3][3];
+		String result = "";
 
-
-
-
-
-
-
-
-
-
-
-
-		return "";
+		if(mat1[0][0]== mat1[0][1] && mat1[0][1] == mat1[0][2])
+			result.equals("" + mat1[0][0] +" is the winner horizontally");
+		else if(mat1[1][0]== mat1[1][1] && mat1[1][1] == mat1[1][2])
+			result.equals("" + mat1[1][0] +" is the winner horizontally");
+		else if(mat1[2][0]== mat1[2][1] && mat1[2][1] == mat1[2][2])
+			result.equals("" + mat1[2][0] +" is the winner horizontally");
+		else if(mat1[0][0]== mat1[1][0] && mat1[1][0] == mat1[2][0])
+			result.equals("" + mat1[0][0] +" is the winner verticallly");
+		else if(mat1[0][1]== mat1[1][1] && mat1[1][1] == mat1[2][1])
+			result.equals("" + mat1[1][1] +" is the winner verticallly");
+		else if(mat1[0][2]== mat1[1][2] && mat1[1][2] == mat1[2][2])
+			result.equals("" + mat1[0][2] +" is the winner verticallly");
+		else if(mat1[0][0]== mat1[1][1] && mat1[1][1] == mat1[2][2])
+			result.equals("" + mat1[1][1] +" is the winner diagonally");
+		else if(mat1[2][0]== mat1[1][1] && mat1[1][1] == mat1[0][2])
+			result.equals("" + mat1[2][0] +" is the winner diagonally");
+		else
+		result.equals("The game is a tie");
+		mat1 = mat;
+		return "" + result;
 	}
 
 	public String toString()
 	{
 		String output="";
+	for(int row = 0; row < mat.length; row++)
+    {
+      System.out.println(Arrays.toString(mat[row]));
+    }
 
-
-
-
+		
 
 
 		return output+"\n\n";
